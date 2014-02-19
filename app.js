3a0fbb748909a0e136cda12dbd3feab61837b2fe
Vince Elizaga <vncelizaga@gmail.com>
2014-02-19 13:43:02 +0800

    update

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 /**
  * Module dependencies.
  */
@@ -9,81 +8,145 @@ var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
 var serialport = require("serialport").SerialPort;
+var github = require('octonode');
+var say = require('say');
 
-var portName = '/dev/tty.Bluetooth-Modem'; //This is the standard Raspberry Pi Serial port
- 
-var readData = ''; //Array to hold the values read in from the port
-var sendData = "0";
-var sendData1 = "0";
-var sendData2 = "0";
-var sendDataTemp = "27";
+var client = github.client({
+    username: 'vynci',
+    password: 'xxx'
+});
+
+var portName = '/dev/tty.usbmodem1421'; 
+
 var cnt = 0;
- 
+var cnt1 = 0;
+var notifcationFlag = 0;
+var tmp;
+
 var sp = new serialport(portName, {
-  baudRate: 9600,
-  dataBits: 8,
-  parity: 'none',
-  stopBits: 1,
-  flowControl: false
+    baudRate: 9600,
+    dataBits: 8,
+    parity: 'none',
+    stopBits: 1,
+    flowControl: false
 });
 
 var app = express();
- 
-app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public')));
+
+app.configure(function() {
+    app.set('port', process.env.PORT || 7000);
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.use(express.favicon());
+    app.use(express.logger('dev'));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(path.join(__dirname, 'public')));
 });
- 
-app.configure('development', function(){
-  app.use(express.errorHandler());
+
+app.configure('development', function() {
+    app.use(express.errorHandler());
 });
- 
+
 app.get('/', routes.index);
 app.get('/users', user.list);
- 
+
 /**
  * Actual Server Code
  */
 
-var server = http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
+var server = http.createServer(app).listen(app.get('port'), function() {
+    console.log("Express server listening on port " + app.get('port'));
 });
 
-app.get('/', function(req, res) {
-  res.json(quotes);
-});
- 
 var io = require('socket.io').listen(server);
- 
-  /**
+
+/**
  * Server and socket started, below are all my listeners and emitters
  */
- 
-io.sockets.on('connection', function(socket){
-  console.log("Socket connected"); 
-  socket.emit('connected', 123);
 
-  socket.on('sendSerial', function(data) {
-  console.log(data); 
-	   sp.write(data);
-  });
- 
-  sp.on('close', function (err) {
-    console.log('port closed');
-  });
- 
-  sp.on('error', function (err) {
-    console.error("error", err);
-  });
- 
-  sp.on('open', function () {
-    console.log('port opened...');
-  });
+
+
+// client.limit(function(err, left, max) {
+//     console.log(left);
+//     console.log(max);
+// });
+
+setInterval(function() {
+    //client.get('/repos/School-Improvement-Network/platform/pulls', {}, function(err, status, body, headers) {
+    client.get('/repos/School-Improvement-Network/backbone.content-navigation/pulls', {}, function(err, status, body, headers) {    
+        console.log(body); //json object
+        if (body.length > 0) {
+            sp.write('d');
+            console.log("Pull!");
+            cnt1 = 0;
+            sp.write('a');
+
+            if (cnt < 2) {
+                sp.write('0');
+                notifcationFlag = 1;                               
+            }
+            if(cnt < 1){
+                if(body[0].user.login === 'maodes'){
+                    body[0].user.login = 'mao-des';
+                }
+                if(body[0].user.login === 'vjcagay'){
+                    body[0].user.login = 'vj kaggay';
+                }
+                if(body[0].user.login === 'ronald-gemao07'){
+                    body[0].user.login = 'ronald gih mao';
+                }
+                if(body[0].user.login === 'vynci'){
+                    body[0].user.login = 'vinci';
+                }                 
+                say.speak('Vicki', 'Pull request by, ' + body[0].user.login);
+                tmp = body[0].user.login;
+            }
+            cnt++;
+        } 
+        else if(body.length == undefined){
+            console.log("beep boop!");
+        }
+        else {
+            console.log("Empty");            
+            sp.write('b');
+            cnt = 0;
+            if (notifcationFlag == 1) {
+                sp.write('1');
+                notifcationFlag = 0;
+                say.speak('Vicki', tmp + ', Pull request closed!');
+            }
+            if (cnt1 < 5) {
+                sp.write('c');
+            } else {
+                sp.write('d');
+            }
+            cnt1++;
+
+        }
+    });
+
+}, 9000);
+
+io.sockets.on('connection', function(socket) {
+    console.log("Socket connected");
+    socket.emit('connected', 123);
+
+    socket.on('sendSerial', function(data) {
+        console.log(data);
+        sp.write(data);
+    });
+
+    sp.on('close', function(err) {
+        console.log('port closed');
+    });
+
+    sp.on('error', function(err) {
+        console.error("error", err);
+    });
+
+    sp.on('open', function() {
+        console.log('port opened...');
+    });
 });
--- arduino/firmware/firmware.ino
+++ arduino/firmware/firmware.ino
@@ -3,12 +3,27 @@ int ledPin13 = 13;
 int ledPin12 = 12; 
 int ledPin11 = 11; 
 int ledPin10 = 10;
-int incomingByte;               
+int ledPin9 = 9; 
+int ledPin8 = 8; 
+int ledPin7 = 7; 
+int ledPin6 = 6;
+int ledPin5 = 5; 
+int ledPin4 = 4; 
+int ledPin3 = 3; 
+int ledPin2 = 2;
+
+int buzzer = A0;
+
+int incomingByte;
+int brightness = 0;    // how bright the LED is
+int fadeAmount = 5;    // how many points to fade the LED by
  
 int ledState = LOW;
 long previousMillis = 0;
 long interval = 1000;
-int blinkEnable = 0;
+
+int notifEnable = 0;
+int notifDisable = 0;
 
 void setup() {
  
@@ -18,8 +33,70 @@ void setup() {
  pinMode(ledPin12, OUTPUT);  
  pinMode(ledPin11, OUTPUT);  
  pinMode(ledPin10, OUTPUT);  
+ pinMode(ledPin9, OUTPUT);  
+ pinMode(ledPin8, OUTPUT);  
+ pinMode(ledPin7, OUTPUT);  
+ pinMode(ledPin6, OUTPUT); 
+ pinMode(ledPin5, OUTPUT);  
+ pinMode(ledPin4, OUTPUT);  
+ pinMode(ledPin3, OUTPUT);  
+ pinMode(ledPin2, OUTPUT); 
+ pinMode(buzzer, OUTPUT); 
+
 }
- 
+
+void ledsOff(){
+ digitalWrite(ledPin13, LOW); 
+ digitalWrite(ledPin12, LOW); 
+ digitalWrite(ledPin11, LOW); 
+ digitalWrite(ledPin10, LOW);  
+ digitalWrite(ledPin9, LOW); 
+ digitalWrite(ledPin8, LOW); 
+ digitalWrite(ledPin7, LOW); 
+ digitalWrite(ledPin6, LOW);
+ digitalWrite(ledPin5, LOW); 
+ digitalWrite(ledPin4, LOW); 
+ digitalWrite(ledPin3, LOW); 
+ digitalWrite(ledPin2, LOW);   
+}
+
+void ledsOn(){
+ digitalWrite(ledPin13, HIGH); 
+ digitalWrite(ledPin12, HIGH); 
+ digitalWrite(ledPin11, HIGH); 
+ digitalWrite(ledPin10, HIGH);  
+ digitalWrite(ledPin9, HIGH); 
+ digitalWrite(ledPin8, HIGH); 
+ digitalWrite(ledPin7, HIGH); 
+ digitalWrite(ledPin6, HIGH);
+ digitalWrite(ledPin5, HIGH); 
+ digitalWrite(ledPin4, HIGH); 
+ digitalWrite(ledPin3, HIGH); 
+ digitalWrite(ledPin2, HIGH);   
+}
+
+void buzzerOn1(){
+ digitalWrite(buzzer, HIGH); 
+ delay(250);
+ digitalWrite(buzzer, LOW);
+ delay(100);  
+ digitalWrite(buzzer, HIGH); 
+ delay(250);
+ digitalWrite(buzzer, LOW);
+ delay(100);
+ digitalWrite(buzzer, HIGH);
+ delay(250);
+ digitalWrite(buzzer, LOW);
+}
+
+void buzzerOn2(){
+ digitalWrite(buzzer, HIGH); 
+ delay(250);
+ digitalWrite(buzzer, LOW);
+ delay(100);  
+}
+
+
 void loop() {
   
  unsigned long currentMillis = millis();
@@ -27,45 +104,22 @@ void loop() {
 // if (Serial.available() > 0) {
    
   incomingByte = Serial.read(); 
-   
-  if (incomingByte == '0') {
-    digitalWrite(ledPin13, HIGH); 
-  }
-  
-  else if (incomingByte == '1') {
-    digitalWrite(ledPin13, LOW); 
-  }
- 
-  else if (incomingByte == '2') {
-    digitalWrite(ledPin12, HIGH); 
-  } 
- 
- 
-  else if (incomingByte == '3') {
-    digitalWrite(ledPin12, LOW); 
-  }
 
-  else if (incomingByte == '4') {
-    digitalWrite(ledPin11, HIGH); 
-  }  
-
-  else if (incomingByte == '5') {
-     digitalWrite(ledPin11, LOW); 
-  }
-
-  else if (incomingByte == '6') {
-     digitalWrite(ledPin13, HIGH); 
-     digitalWrite(ledPin12, HIGH); 
-     digitalWrite(ledPin11, HIGH); 
-     digitalWrite(ledPin10, HIGH); 
+  if (incomingByte == '6') {
+     ledsOn();
   } 
 
   else if (incomingByte == '7') {
-     digitalWrite(ledPin13, LOW); 
-     digitalWrite(ledPin12, LOW); 
-     digitalWrite(ledPin11, LOW); 
-     digitalWrite(ledPin10, LOW); 
+     ledsOff();
+  }
+ 
+  else if (incomingByte == '0') {
+     buzzerOn1();
   } 
+
+  else if (incomingByte == '1') {
+     buzzerOn2();
+  }   
 
   else if (incomingByte == '9') {
      digitalWrite(ledPin10, HIGH);
@@ -78,28 +132,174 @@ void loop() {
   } 
 
   else if (incomingByte == 'a') {
-     blinkEnable = 1;
+     notifEnable = 1;
   }
 
   else if (incomingByte == 'b') {
-     blinkEnable = 0;
-     digitalWrite(ledPin12, LOW);
+     notifEnable = 0;
+     ledsOff();
   }  
-  
-  if(blinkEnable == 1){
-    if(currentMillis - previousMillis > interval) {
-        // save the last time you blinked the LED 
-        previousMillis = currentMillis;   
-    
-        // if the LED is off turn it on and vice-versa:
-        if (ledState == LOW)
-          ledState = HIGH;
-        else
-          ledState = LOW;
-    
-        // set the LED with the ledState of the variable:
-        digitalWrite(ledPin12, ledState);
-      }  
+
+  else if (incomingByte == 'c') {
+     notifDisable = 1;
   }
- //}
+
+  else if (incomingByte == 'd') {
+     notifDisable = 0;
+     ledsOff();
+  }
+
+  ////LED INDIVIDUAL CONTROL////
+
+  else if (incomingByte == ',') {
+    digitalWrite(ledPin13, HIGH); 
+  }  
+
+  else if (incomingByte == '.') {
+    digitalWrite(ledPin13, LOW);
+  }
+
+  else if (incomingByte == 'e') {
+    digitalWrite(ledPin12, HIGH);
+  } 
+
+  else if (incomingByte == 'f') {
+    digitalWrite(ledPin12, LOW);
+  }  
+
+  else if (incomingByte == 'g') {
+    digitalWrite(ledPin11, HIGH);
+  }
+
+  else if (incomingByte == 'h') {
+    digitalWrite(ledPin11, LOW);
+  }
+
+  else if (incomingByte == 'i') {
+    digitalWrite(ledPin10, HIGH); 
+  }  
+
+  else if (incomingByte == 'j') {
+    digitalWrite(ledPin10, LOW);
+  }
+
+  else if (incomingByte == 'k') {
+    digitalWrite(ledPin9, HIGH);
+  } 
+
+  else if (incomingByte == 'l') {
+    digitalWrite(ledPin9, LOW);
+  }  
+
+  else if (incomingByte == 'm') {
+    digitalWrite(ledPin8, HIGH);
+  }
+
+  else if (incomingByte == 'n') {
+    digitalWrite(ledPin8, LOW);
+  }
+
+  else if (incomingByte == 'o') {
+    digitalWrite(ledPin7, HIGH); 
+  }  
+
+  else if (incomingByte == 'p') {
+    digitalWrite(ledPin7, LOW);
+  }
+
+  else if (incomingByte == 'q') {
+    digitalWrite(ledPin6, HIGH);
+  } 
+
+  else if (incomingByte == 'r') {
+    digitalWrite(ledPin6, LOW);
+  }  
+
+  else if (incomingByte == 's') {
+    digitalWrite(ledPin5, HIGH);
+  }
+
+  else if (incomingByte == 't') {
+    digitalWrite(ledPin5, LOW);
+  }    
+
+  else if (incomingByte == 'u') {
+    digitalWrite(ledPin4, HIGH); 
+  }  
+
+  else if (incomingByte == 'v') {
+    digitalWrite(ledPin4, LOW);
+  }
+
+  else if (incomingByte == 'w') {
+    digitalWrite(ledPin3, HIGH);
+  } 
+
+  else if (incomingByte == 'x') {
+    digitalWrite(ledPin3, LOW);
+  }  
+
+  else if (incomingByte == 'y') {
+    digitalWrite(ledPin2, HIGH);
+  }
+
+  else if (incomingByte == 'z') {
+    digitalWrite(ledPin2, LOW);
+  }   
+
+  ////PWM BLINK LOGIC////     
+  
+  if(notifEnable == 1){
+    analogWrite(ledPin13, brightness);
+    analogWrite(ledPin11, brightness);    
+
+    analogWrite(ledPin10, brightness);
+    analogWrite(ledPin8, brightness);    
+
+    analogWrite(ledPin7, brightness);
+    analogWrite(ledPin5, brightness);    
+
+    analogWrite(ledPin4, brightness);
+    analogWrite(ledPin2, brightness);
+  
+    // change the brightness for next time through the loop:
+    brightness = brightness + fadeAmount;
+  
+    // reverse the direction of the fading at the ends of the fade: 
+    if (brightness == 0 || brightness == 255) {
+      if(brightness == 0){
+        ledsOff();
+      }      
+      fadeAmount = -fadeAmount ; 
+      delay(150);
+    }     
+    // wait for 30 milliseconds to see the dimming effect    
+    delay(15);
+  }
+
+  if(notifDisable == 1){
+    analogWrite(ledPin12, brightness);
+
+    analogWrite(ledPin9, brightness);
+
+    analogWrite(ledPin6, brightness);
+
+    analogWrite(ledPin3, brightness);
+  
+    // change the brightness for next time through the loop:
+    brightness = brightness + fadeAmount;
+  
+    // reverse the direction of the fading at the ends of the fade: 
+    if (brightness == 0 || brightness == 255) {
+      if(brightness == 0){
+        ledsOff();
+      }
+      fadeAmount = -fadeAmount ; 
+      delay(150);
+    }     
+    // wait for 30 milliseconds to see the dimming effect    
+    delay(15);
+  }
+
+
 }
